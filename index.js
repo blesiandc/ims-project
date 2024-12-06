1579b100daeb104e3c5a651e3d633a10b0047d7e
blesiandc <blessieannedelacruz@gmail.com>
2024-12-06 15:34:25 +0800

    Added bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
+
+app.get("/items", (req, res) => {
+  const sample = "Hello Bless";
+  console.log(sample);
+  //   res.send("Hello");
+  res.render("index", { sample });
+});
 
 app.listen(3000, () => {
   console.log("Running on port 3000");
