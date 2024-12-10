3f019e80bce9bc79cb345346ff3f8b9516a2a5ca
blesiandc <blessieannedelacruz@gmail.com>
2024-12-10 20:26:34 +0800

    refactor

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -18,9 +18,12 @@ exports.addItem = async (req, res) => {
   try {
     const item = new Items(req.body);
     await item.save();
-    res.status(200).json({
-      message: "Item created successfully.",
-    });
+
+    // res.status(200).json({
+    //   message: "Item created successfully.",
+    //   item,
+    // });
+    res.redirect("/items");
   } catch (err) {
     res.status(500).json({ err: "An error occurred while adding the item." });
   }
@@ -52,18 +55,13 @@ exports.editForm = async (req, res) => {
 
 exports.updateItem = async (req, res) => {
   const id = req.params.id;
-  const { name, category, quantity, price, description } = req.body;
-
   try {
-    const updatedItem = await Items.findByIdAndUpdate(
-      id,
-      { name, category, quantity, price, description },
-      { new: true }
-    );
-
-    res.status(200).json({
-      message: "Item updated successfully.",
-    });
+    const updatedItem = await Items.findByIdAndUpdate(id, { ...req.body });
+    // res.status(200).json({
+    //   message: "Item updated successfully.",
+    //   updatedItem,
+    // });
+    res.redirect(`/items/${id}`);
   } catch (err) {
     res.status(500).json({ err: "An error occurred while editing the item." });
   }
@@ -77,7 +75,9 @@ exports.deleteItem = async (req, res) => {
     if (!deletedItem) {
       return res.status(404).json({ message: "Item not found" });
     }
-
+    // res.status(200).json({
+    //   message: "Item deleted successfully.",
+    // });
     res.redirect("/items");
   } catch (err) {
     res.status(500).json({ err: "An error occurred while deleting the item" });
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -3,6 +3,7 @@ const itemController = require("../controllers/itemController");
 const router = express.Router();
 
 router.get("/", itemController.getAllItems);
+
 router.get("/addForm", itemController.addForm);
 router.post("/add", itemController.addItem);
 
@@ -10,5 +11,7 @@ router.get("/:id", itemController.viewItem);
 
 router.get("/:id/edit", itemController.editForm);
 router.put("/:id", itemController.updateItem);
+
 router.delete("/:id", itemController.deleteItem);
+
 module.exports = router;
